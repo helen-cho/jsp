0daaf172be6ee9963ebdb89a8207307b8a5a4348
helen-cho <hd9536@daum.net>
2024-05-02 11:20:57 +0900

    목록

--- ex04/src/main/java/controller/BBSServlet.java
+++ ex04/src/main/java/controller/BBSServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,17 +10,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 
-@WebServlet(value={"/bbs/list", "/bbs/insert", "/bbs/read", "/bbs/delete", "/bbs/update"})
+import model.*;
+
+@WebServlet(value={"/bbs/list.json", "/bbs/list", "/bbs/insert", "/bbs/read", "/bbs/delete", "/bbs/update"})
 public class BBSServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    BBSDAOImpl dao=new BBSDAOImpl();
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out=response.getWriter();
 		RequestDispatcher dis=request.getRequestDispatcher("/home.jsp");
+		
 		switch(request.getServletPath()) {
 		case "/bbs/list":
 			request.setAttribute("pageName", "/bbs/list.jsp");
 			dis.forward(request, response);
+			break;
+		case "/bbs/list.json":
+			Gson gson=new Gson();
+			out.print(gson.toJson(dao.list()));
 			break;
 		}
 	}
