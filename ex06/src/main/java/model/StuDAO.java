c566d25b5e9acfc78c5e449ef1f611bf14ca2249
helen-cho <hd9536@daum.net>
2024-05-08 12:01:44 +0900

    학생목록

---
+++ ex06/src/main/java/model/StuDAO.java
@@ -0,0 +1,10 @@
+package model;
+import java.util.*;
+
+public interface StuDAO {
+	//학생목록
+	public ArrayList<StuVO> list(QueryVO vo);
+	
+	//검색수
+	public int total(QueryVO vo);
+}
---
+++ ex06/src/main/java/model/StuDAOImpl.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.util.ArrayList;
+import java.sql.*;
+
+public class StuDAOImpl implements StuDAO{
+	Connection con=Database.CON;
+	
+	@Override
+	public ArrayList<StuVO> list(QueryVO vo) {
+		ArrayList<StuVO> array=new ArrayList<StuVO>();
+		try {
+			String sql="select * from view_stu";
+			sql += " where " + vo.getKey() +  " like ?";
+			sql += " order by scode desc";
+			sql += " limit ?, ?";
+			PreparedStatement ps=con.prepareStatement(sql);
+			ps.setString(1, "%" + vo.getWord() + "%");
+			ps.setInt(2, (vo.getPage()-1) * vo.getSize());
+			ps.setInt(3, vo.getSize());
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()) {
+				StuVO stu=new StuVO();
+				stu.setScode(rs.getString("scode"));
+				stu.setSname(rs.getString("sname"));
+				stu.setDept(rs.getString("dept"));
+				stu.setAdvisor(rs.getString("advisor"));
+				stu.setPname(rs.getString("pname"));
+				stu.setBirthday(rs.getString("birthday"));
+				stu.setYear(rs.getInt("year"));
+				System.out.println(stu.toString());
+				array.add(stu);
+			}
+		}catch(Exception e) {
+			System.out.println("학생목록:" + e.toString());
+		}
+		return array;
+	}
+
+	@Override
+	public int total(QueryVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
